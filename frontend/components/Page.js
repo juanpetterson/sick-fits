ee86e42b9f53f9491b7c9008fc9fa14f89cd9fef
juanpetterson <juan.petterson@gmail.com>
2020-07-06 11:26:40 -0300

    Styled header and main page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Header from '../components/Header';
 import Meta from '../components/Meta';
 
-const MyButton = styled.button`
-  background: red;
-  font-size: 100px;
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offwhite: '#EDEDED',
+  maxWidth: '1000px',
+  boxshadow: '0 12px 24px 0 rgba(0,0,0,0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        <MyButton>Click Me</MyButton>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
