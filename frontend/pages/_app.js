cc78a45160da51d2d66fe8b7291bef1545d3ae35
juanpetterson <juan.petterson@gmail.com>
2020-07-08 22:44:07 -0300

    configure apollo client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,36 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
 
+import withData from '../lib/withData';
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // This expose the query to the user
+    pageProps.query = ctx.query;
+
+    // Returning something here the next.js will expose it via props
+    // this static method execute earlier than the render
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
