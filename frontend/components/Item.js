defe04bbb7cdffa89fad7654fb4305bed11c75a4
juanpetterson <juan.petterson@gmail.com>
2020-07-09 22:13:32 -0300

    Add items list

---
+++ frontend/components/Item.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+
+class componentName extends Component {
+  render() {
+    const { item } = this.props;
+
+    return (
+      <ItemStyles>
+        {item.image && <img src={item.image} alt={item.title} />}
+        <Title>
+          <Link href={`/item?id${item.id}`}>
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+        <div className='buttonList'>
+          <Link href={{ pathname: '/item', query: { id: item.id } }}>
+            <a>Edit</a>
+          </Link>
+        </div>
+      </ItemStyles>
+    );
+  }
+}
+
+export default componentName;
