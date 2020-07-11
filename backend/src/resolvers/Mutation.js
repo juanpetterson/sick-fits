0904e893b20e61bf97e099168254fd74105db0f9
juanpetterson <juan.petterson@gmail.com>
2020-07-11 16:04:01 -0300

    add updateItem and deleteItem mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,10 +10,35 @@ const Mutations = {
         },
       },
       info
-    )
+    );
 
-    return item
+    return item;
   },
-}
+  updateItem(parent, args, context, info) {
+    // first take a copu of the updates
+    const updates = { ...args };
+    //remove the id from the updates
+    delete updates.id;
+    // run the update method
+    return context.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, context, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await context.db.query.item({ where }, `{id title}`);
+    // check if they own that item or have the permissions
+    // TODO
+    // delete it
+    return context.db.mutation.deleteItem({ where }, info);
+  },
+};
 
-module.exports = Mutations
+module.exports = Mutations;
