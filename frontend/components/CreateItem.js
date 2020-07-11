6bc82328a22dc23ca9c8b6202c885a050b03734b
juanpetterson <juan.petterson@gmail.com>
2020-07-11 14:06:55 -0300

    add submit item function

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import ErrorMesasge from '../components/ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -19,52 +41,60 @@ class CreateItem extends Component {
     this.setState({ ...this.state, [name]: parsedValue });
   };
 
-  handleSubmit = event => {
-    console.log(event);
+  handleSubmit = async (event, callback) => {
+    event.preventDefault();
+    const response = await callback();
+    Router.push({ pathname: '/item', query: { id: response.data.createItem.ud } });
   };
 
   render() {
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <fieldset>
-          <label htmlFor='title'>
-            Title
-            <input
-              value={this.state.title}
-              onChange={this.handleChange}
-              type='text'
-              id='title'
-              name='title'
-              placeholder='Title'
-              required></input>
-          </label>
-          <label htmlFor='price'>
-            Price
-            <input
-              id='price'
-              type='number'
-              name='price'
-              placeholder='Price'
-              min={0}
-              value={this.state.price}
-              onChange={this.handleChange}
-              required></input>
-          </label>
-          <label htmlFor='description'>
-            Description
-            <textarea
-              id='description'
-              name='description'
-              placeholder='Description'
-              value={this.state.description}
-              onChange={this.handleChange}
-              required></textarea>
-          </label>
-          <button type='submit'>Submit</button>
-        </fieldset>
-      </Form>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error, called, data }) => (
+          <Form onSubmit={event => this.handleSubmit(event, createItem)}>
+            <ErrorMesasge error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor='title'>
+                Title
+                <input
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  type='text'
+                  id='title'
+                  name='title'
+                  placeholder='Title'
+                  required></input>
+              </label>
+              <label htmlFor='price'>
+                Price
+                <input
+                  id='price'
+                  type='number'
+                  name='price'
+                  placeholder='Price'
+                  min={0}
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                  required></input>
+              </label>
+              <label htmlFor='description'>
+                Description
+                <textarea
+                  id='description'
+                  name='description'
+                  placeholder='Description'
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                  required></textarea>
+              </label>
+              <button type='submit'>Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     );
   }
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
