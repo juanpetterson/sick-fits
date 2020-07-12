7987f76c9f5848f01dbc7a3af694034bcb502097
juanpetterson <juan.petterson@gmail.com>
2020-07-11 21:51:45 -0300

    add signin mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -66,6 +66,26 @@ const Mutations = {
 
     return user;
   },
+  async signin(parent, { email, password }, context, info) {
+    const user = await context.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    context.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    return user;
+  },
 };
 
 module.exports = Mutations;
