d0c9366e6842d26fd06f9793e4d4d216f1f5f825
juanpetterson <juan.petterson@gmail.com>
2020-07-12 19:49:26 -0300

    add user permissions page

---
+++ frontend/components/Permissions.js
@@ -0,0 +1,81 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import ErrorMessage from './ErrorMessage';
+import Table from './styles/Table';
+import SickButton from './styles/SickButton';
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
+];
+
+const ALL_USERS_QUERY = gql`
+  query ALL_USERS_QUERY {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <div>
+        <ErrorMessage error={error} />
+        <div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(permission => (
+                  <th>{permission}</th>
+                ))}
+                <th>👇🏼</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.users.map(user => (
+                <User user={user} />
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    )}
+  </Query>
+);
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user;
+
+    return (
+      <tr>
+        <td style={{ fontSize: '12px' }}>{user.name}</td>
+        <td style={{ fontSize: '12px' }}>{user.email}</td>
+        {possiblePermissions.map(permission => (
+          <td style={{ textAlign: 'center' }}>
+            <label htmlFor={`${user.id}-permission-${permission}`}>
+              <input type='checkbox' />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Permissions;
